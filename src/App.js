c794bacac11577c7ecfae2d1186834d91f396824
Gabriel Delight <gabrieldelight08@gmail.com>
2023-11-30 15:16:31 +0100

    Init commit

--- src/App.js
+++ src/App.js
@@ -5,9 +5,15 @@ import notifee from "@notifee/react-native";
 const App = () => {
   async function onTriggerHandler(){
     await notifee.displayNotification({
-      id: "1234",
-      title: `New notification`,
-      body: "Greetings! How are you faring today?",
+      id: "1",
+      title: `New message in chat`,
+      body: "You received a new message from Mike.",
+    });
+
+    await notifee.displayNotification({
+      id: "2",
+      title: `App update`,
+      body: "Please update your app from the App Store as it is outdated.",
     });
   }
 
@@ -15,7 +21,10 @@ const App = () => {
   return (
     <View style={styles.body}>
       <TouchableOpacity style={styles.button} onPress={onTriggerHandler} >
-        <Text style={styles.btn_text}>Greet!</Text>
+        <Text style={styles.btn_text}>New message</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={onTriggerHandler} >
+        <Text style={styles.btn_text}>App update</Text>
       </TouchableOpacity>
     </View>
   )
@@ -25,10 +34,11 @@ export default App
 
 const styles = StyleSheet.create({
   body: {
-    backgroundColor: "black",
+    backgroundColor: "#ddd",
     flex: 1,
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
+    justifyContent: "space-around"
   },
   button: {
     width: 200,
