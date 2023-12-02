f8725d77d9a6ed3f2e982ae1d789cc7bf0ca7071
Gabriel Delight <53006140+GabrielDelight@users.noreply.github.com>
2023-12-02 17:34:11 +0100

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
 import notifee from "@notifee/react-native";
 
 const App = () => {
-  async function onTriggerHandler(){
+  async function onTriggerHandler() {
     await notifee.displayNotification({
       id: "1",
       title: `New message in chat`,
@@ -17,20 +17,16 @@ const App = () => {
     });
   }
 
-
   return (
     <View style={styles.body}>
-      <TouchableOpacity style={styles.button} onPress={onTriggerHandler} >
-        <Text style={styles.btn_text}>New message</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={onTriggerHandler} >
-        <Text style={styles.btn_text}>App update</Text>
-      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={onTriggerHandler}>
+        <Text style={styles.btn_text}>Display</Text>
+      </TouchableOpacity> 
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
 const styles = StyleSheet.create({
   body: {
@@ -38,17 +34,17 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    justifyContent: "space-around"
+    justifyContent: "space-around",
   },
   button: {
     width: 200,
     backgroundColor: "hotpink",
     padding: 20,
     alignItems: "center",
-    borderRadius: 20
+    borderRadius: 20,
   },
   btn_text: {
     fontSize: 20,
-    color: "white"
-  }
-})
+    color: "white",
+  },
+});
